21fa3a39d63020a6bc41ff4589663f7b90b45c1a
Todd <facecat007@163.com>
2022-12-31 16:14:00 +0800

    1

--- future.py
+++ future.py
@@ -65,16 +65,22 @@ def setAttribute(view, child):
 				lowerStr = value.lower()
 				if(lowerStr.find("rgb") == 0):
 					view.m_backColor = value
+				else:
+					view.m_backColor = "none"
 			elif(name == "bordercolor"):
 				lowerStr = value.lower()
 				if(lowerStr.find("rgb") == 0):
 					view.m_borderColor = value
+				else:
+					view.m_borderColor = "none"
 			elif(name == "textcolor"):
 				lowerStr = value.lower()
 				if(lowerStr.find("rgb") == 0):
 					view.m_textColor = value
+				else:
+					view.m_textColor = "none"
 			elif(name == "layoutstyle"):
-				view.m_layoutStyle = value
+				view.m_layoutStyle = value.lower()
 			elif(name == "dock"):
 				view.m_dock = value;
 			elif(name == "font"):
@@ -85,7 +91,7 @@ def setAttribute(view, child):
 			elif(name == "headerheight"):
 				view.m_headerHeight = float(value)
 			elif(name == "splitmode"):
-				view.m_splitMode = value
+				view.m_splitMode = value.lower()
 			elif(name == "autowrap"):
 				view.m_autoWrap = (value.lower() == "true")
 			elif(name == "name"):
@@ -110,8 +116,6 @@ def setAttribute(view, child):
 				view.m_src = value
 			elif(name == "backimage"):
 				view.m_backImage = value
-			elif(name == "groupname"):
-				view.m_groupName = value
     
 #读取Xml
 #paint 绘图对象
